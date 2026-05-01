cbd36045a642b4482f6ee48d5aef9cf7555d05e6
Ayush Joshi <realayushjoshi@gmail.com>
2026-05-01 19:17:54 +0000

    DAY20-21

--- client/src/app/bank/components/account/account.component.ts
+++ client/src/app/bank/components/account/account.component.ts
@@ -1,5 +1,34 @@
+// src/app/accounts/accounts.component.ts
+import { Component, OnInit } from '@angular/core';
+import { AccountTS } from '../../types/tstypes/Accountts';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-export class AccountComponent {
- 
+@Component({
+  selector: 'app-accounts',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.scss']
+})
+export class AccountComponent implements OnInit {
+  accountForm!: FormGroup;
+  account: AccountTS | undefined;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    // No need to fetch data from a service since we are using hardcoded data
+    this.accountForm = this.fb.group({
+      accountId: ["", [Validators.required, Validators.min(1)]],
+      customerId: ["", [Validators.required]],
+      balance: ["", [Validators.required, Validators.min(0)]],
+    });
+    this.account = new AccountTS("1", 1000.00, "1");
+  }
+
+  onSubmit(): void {
+    if (this.accountForm.valid) {
+      console.log("Form Submitted:", this.accountForm.value);
+    } else {
+      console.log("Form is invalid. Please fix the errors.");
+    }
+  }
 }
